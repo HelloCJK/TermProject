bb7a8ca2e39d2b02f72bf82aa6255639dcb35169
NayongLee <iny003@naver.com>
2016-12-16 20:11:39 +0900

    Java Socket Server is almost complete

--- JavaSocketServer/src/com/game/JavaSocket/JavaSocketServer.java
+++ JavaSocketServer/src/com/game/JavaSocket/JavaSocketServer.java
@@ -1,9 +1,11 @@
 package com.game.JavaSocket;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,16 +41,30 @@ public class JavaSocketServer implements Runnable {
 					InputStream in = socket.getInputStream(); // 클라이언트 소켓의 바이트
 					BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-					OutputStream out = socket.getOutputStream(); 
+					OutputStream out = socket.getOutputStream();
 					PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
 
-					while(true){
-						char[] buf = new char[20];
+					while (true) {
+						char[] buf = new char[1024];
+
+						br.read(buf, 0, 1024);
+						if(buf==null) break;
+						String recive = String.valueOf(buf);
+						System.out.println(getTime() + " 클라이언트로부터 받은 문자열 : " + recive);
+						if (recive == "s") {
+							System.out.println(getTime()+"저장중입니다.");
+							br.read(buf, 0, 1024);
+							String data = String.valueOf(buf);
+							writeFile(data);
+							System.out.println(getTime()+"저장완료.");
+						}
+						if (recive == "l"){
+							System.out.println(getTime()+"데이터를 저장합니다.");
+							buf = stringFile().toCharArray();
+							pw.write(buf, 0, 1024);
+							pw.flush(); // 버퍼를 비움
+						}						
 						
-						br.read(buf,0,20);
-						System.out.println(getTime() + " 클라이언트로부터 받은 문자열 : " + String.valueOf(buf));
-						pw.write(buf, 0,20);
-						pw.flush(); // 버퍼를 비움
 					}
 				} catch (Exception e) {
 					System.out.println(getTime() + " Error");
@@ -64,29 +80,48 @@ public class JavaSocketServer implements Runnable {
 		}
 
 	}
-	public String stringFile(){
-	      String out = null;
-	      File m_file = new File("E:\\TermProJect00\\TermProject\\test.txt");
-	      try {
-	         FileReader m_reader = new FileReader(m_file);
-	         int c = 0;
-	         while(true){
-	            c = m_reader.read();
-	            if(c==-1)   break;
-	            if(out == null)   out = String.valueOf((char)c);
-	            else         out += String.valueOf((char)c);
-	         }
-	         m_reader.close();
-	      } catch (FileNotFoundException e) {
-	         // TODO Auto-generated catch block
-	         e.printStackTrace();
-	      } catch (IOException e) {
-	         // TODO Auto-generated catch block
-	         e.printStackTrace();
-	      }
-	      return out;
-	   }
 
+	public String stringFile() {
+		String out = null;
+		File m_file = new File("E:\\TermProJect00\\TermProject\\JavaSocketServer\\test.txt");
+		try {
+			FileReader m_reader = new FileReader(m_file);
+			int c = 0;
+			while (true) {
+				c = m_reader.read();
+				if (c == -1)
+					break;
+				if (out == null)
+					out = String.valueOf((char) c);
+				else
+					out += String.valueOf((char) c);
+			}
+			m_reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return out;
+	}
+
+	public void writeFile(String str) {
+		File m_file = new File("E:\\TermProJect00\\TermProject\\JavaSocketServer\\test.txt");
+		try {
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(m_file));
+			// BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+			bufferedWriter.write(str);
+			bufferedWriter.newLine();
+
+			bufferedWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 
